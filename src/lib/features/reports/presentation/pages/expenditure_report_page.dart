7d2afab09b7c309b9e53b696c80c1bb92a70437c
Programian <ian.rivera21@upr.edu>
2026-05-14 21:22:24 -0400

    further clarified where the radius is

--- src/lib/features/reports/presentation/pages/expenditure_report_page.dart
+++ src/lib/features/reports/presentation/pages/expenditure_report_page.dart
@@ -1,5 +1,5 @@
 //TO DO: REPLACE HARDCODED DATA WITH DATA PULLED FROM BACKEND (SEE LINE 44)
-//Graph radius may require adjustment based on the amount of categories
+//Graph radius in widgets/dynamic_pie_chart.dart may require adjustment based on the amount of categories
 
 import 'dart:math' as math;
 import 'dart:typed_data';
